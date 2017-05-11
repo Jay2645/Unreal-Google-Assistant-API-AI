cb5e53b2d541b9b26efec7d8d6ee710a5c7eb94d
Jay Stevens <disneylandjay@gmail.com>
2017-05-10 19:15:26 -0700

    Added initial Javascript index file.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Unreal Engine 4 Google Assistant API AI Code
 
-This is part of a larger project which aims to integrate the [Google Assistant SDK](https://developers.google.com/assistant/sdk/) into Unreal Engine 4.
+This is [part of a larger project](https://github.com/Jay2645/Unreal-Google-Assistant-API-AI) which aims to integrate the [Google Assistant SDK](https://developers.google.com/assistant/sdk/) into Unreal Engine 4.
 
 This particular section uses [API.AI](https://console.api.ai/api-client) to communicate with the "Actions on Google" API. This is where the Google Assistant "talks" to API.AI, which in turn generates the "Intents" which determine what different voice commands do.
 
---
+++ index.js
@@ -0,0 +1,123 @@
+var request = require('request');
+var pubsub = require('@google-cloud/pubsub');
+var ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
+
+var pubsubClient = pubsub({ projectId: 'compact-moment-166306' });
+var topicName = 'unreal_google_assistant';
+
+exports.webhook = function(req, res) {
+
+  var respData = {
+	'speech': 'This is default speech.',
+	'displayText': 'This is default display text.',
+	'data': {},
+	'contextOut': [],
+	'source': ''
+  };
+
+  var intent = req.body.result.metadata.intentName;
+  console.log('intent = ' + intent);
+
+  switch (intent) {
+	/*case 'prime_pump_start':
+
+	  var whichPump = req.body.result.parameters.which_pump;
+	  console.log('whichPump = ' + whichPump);
+
+	  createTopic(function(topic) {
+		var pubData = { 'intent': 'prime_pump_start', 'which_pump': whichPump };
+		publishMessage(topic, JSON.stringify(pubData), function() {
+		  var s = 'Priming pump ' + whichPump + '.';
+		  respData.speech = s;
+		  respData.displayText = s;
+		  res.json(respData);
+		});
+	  });
+
+	  break;
+
+	case 'prime_pump_end':
+
+	  createTopic(function(topic) {
+
+		var pubData = { 'intent': 'prime_pump_end' };
+
+		publishMessage(topic, JSON.stringify(pubData), function() {
+
+		  var s = 'Stopped priming pump.';
+		  respData.speech = s;
+		  respData.displayText = s;
+		  res.json(respData);
+		});
+	  });
+
+	  break;
+
+	case 'make_drink':
+	  var drink = req.body.result.parameters.drink;
+	  console.log('drink = ' + drink);
+
+	  createTopic(function(topic) {
+		var pubData = { 'intent': 'make_drink', 'drink': drink };
+		publishMessage(topic, JSON.stringify(pubData), function() {
+		  var s = 'Coming right up. While I make your drink, would you like to hear the weather or your fortune?';
+		  respData.speech = s;
+		  respData.displayText = s;
+		  res.json(respData);
+		});
+	  });
+	  break;*/
+	  
+	
+	default:
+	  console.log('switch-case in default');
+	  res.json(respData);
+	  break;
+  }
+};
+
+function createTopic(callback) {
+
+  if (!callback) {
+	console.log('no callback');
+	return;
+  }
+
+  pubsubClient.createTopic(topicName, function(error, topic) {
+
+	// topic already exists
+	if (error && error.code === 409) {
+
+	  console.log('topic created');
+
+	  // callback(topic);
+	  callback(pubsubClient.topic(topicName));
+	  return;
+	}
+
+	if (error) {
+	  console.log(error);
+	  return;
+	}
+
+	callback(pubsubClient.topic(topicName));
+  });
+}
+
+function publishMessage(topic, message, callback) {
+
+  topic.publish(message, function(error) {
+
+	if (error) {
+	  console.log('Publish error:');
+	  console.log(error);
+	  return;
+	}
+
+	console.log('publish successful');
+
+	if (callback) {
+	  callback();
+	}
+  });
+}
